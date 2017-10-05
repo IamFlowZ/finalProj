d794acffcef17e8ed8dd1d331f04287a5a140da5
Kota Lewallen <djlewald21@gmail.com>
2017-10-05 16:41:12 -0400

    can now set the color, can't change the color on the fly yet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,7 +20,7 @@ export class HomePage {
 
 
   constructor(public navCtrl: NavController, private deviceMotion: DeviceMotion) {
-    let subscription = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => { this.model = acceleration; });
+    const subscription = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => { this.model = acceleration; });
     //need to: figure out better offset, set bounds, color changing capbilities after initial method call, need to refine color checker
 
   }
@@ -29,9 +29,12 @@ export class HomePage {
     let canvas: any = document.getElementById('gameSprite');
     let ctx = canvas.getContext("2d");
     let offset = getOffset(canvas);
-    let color = this.color;
+    const currentColor:string [] = ["red", "yellow", "blue", "green"];
+    let yourColor: string;
+    let theColor = this.color;
+    // this.draw("pointermove");
 
-    if(this.color!= null) {
+    if(this.color != null) {
       canvas.addEventListener("pointerdown", function() {
         canvas.addEventListener("pointermove", draw, false);
     }
@@ -40,19 +43,29 @@ export class HomePage {
     }
 
     function endPointer(event) {
-     canvas.removeEventListener("pointermove", draw, false)
+     canvas.removeEventListener("pointermove", false)
      this.lastPt = null;
+    }
+
+    async function colorChecker(color) {
+      for (var i = 0; i < currentColor.length-1; i++) {
+        if (color == currentColor[i]) {
+          yourColor = color;
+        }
+      }
+
     }
     function draw(event) {
       if(this.lastPt!=null) {
+        colorChecker(theColor);
         ctx.beginPath();
-        ctx.strokeStyle = color;
+        ctx.strokeStyle = yourColor;
         ctx.LineWidth = 5;
         ctx.moveTo(this.lastPt.x,this.lastPt.y);
-        ctx.lineTo(event.pageX, event.pageY);
+        ctx.lineTo(event.pageX-offset.left, event.pageY-offset.top);
         ctx.stroke();
       }
-      this.lastPt = {x:event.pageX, y:event.pageY}
+      this.lastPt = {x:event.pageX-offset.left, y:event.pageY-offset.top}
     }
     function getOffset(obj) {
       var offsetLeft = 0;
@@ -70,7 +83,50 @@ export class HomePage {
     function clear() {
       ctx.restore();
     }
+
   }
+
+  // draw(event) {
+  //   let canvas: any;
+  //   let ctx: any;
+
+
+  //   if(this.lastPt!=null) {
+  //     colorChecker(theColor);
+  //     ctx.beginPath();
+  //     ctx.strokeStyle = yourColor;
+  //     ctx.LineWidth = 5;
+  //     ctx.moveTo(this.lastPt.x,this.lastPt.y);
+  //     ctx.lineTo(event.pageX-offset.left, event.pageY-offset.top);
+  //     ctx.stroke();
+  //   }
+  //   this.lastPt = {x:event.pageX-offset.left, y:event.pageY-offset.top}
+
+  //   function getOffset(obj) {
+  //     var offsetLeft = 0;
+  //     var offsetTop = 0;
+  //     do {
+  //       if (!isNaN(obj.offsetLeft)) {
+  //         offsetLeft += obj.offsetLeft;
+  //       }
+  //       if (!isNaN(obj.offsetTop)) {
+  //         offsetTop += obj.offsetTop;
+  //       }
+  //     } while(obj = obj.offsetParent );
+  //     return {left: offsetLeft, top: offsetTop};
+  //   }
+
+  //   function colorChecker(color) {
+  //     for (var i = 0; i < currentColor.length-1; i++) {
+  //       if (color == currentColor[i]) {
+  //         yourColor = color;
+  //       }
+  //     }
+
+  //   }
+
+  // }
+
 
   getColor() {
     if (this.model.x <= 3 && this.model.y >= 6) {
@@ -106,23 +162,29 @@ export class HomePage {
   }
 
   async colorSwapper() {
-    var counter: number;
-    counter +1;
-    if(counter =1 ){
+    //Idea I had for limiting the number of instances that can be ran at once.
+    // var counter: number;
+    // counter +1;
+    // if( counter = 1 ){
+
     this.startTimer();
+
     // this.canvasStart();
-    for (let i = 0; i <= 360; i++) {
+    for (let i = 0; i <= 900; i++) {
       this.getColor();
-      await this.delay(250, i);
+      await this.delay(100, i);
       this.getColor();
     }
+
+
+
     // counter -1;
   }
-  else {console.log("cannot run more than one game instance");}
-
-  }
+  // else {console.log("cannot run more than one game instance");}
 
 }
+
+
 
 
 
